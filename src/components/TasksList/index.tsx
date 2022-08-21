982d5854da6464353bc1659c2403f2791e16e65b
adautomoises <adautomaleandro@gmail.com>
2022-08-21 16:59:35 -0300

    Update TasksList Component

--- src/components/TasksList/index.tsx
+++ src/components/TasksList/index.tsx
@@ -1,52 +1,7 @@
-// import { View, Text, TouchableOpacity} from 'react-native';
-// import React from 'react';
-
-// export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps) {
-  // return(
-    //   <View style={styles.container}>
-    //     <TouchableOpacity style={styles.buttonCheck} onPress={onCheck}>
-    //       <FontAwesome5 name="circle" size={20} color="#4EA8DE" />
-    //     </TouchableOpacity>
-  //     <Text style={styles.taskText}>{name}</Text>
-  //     <TouchableOpacity style={styles.buttonRemove} 
-  //     onPress={onRemove}
-  //     >
-  //       <FontAwesome5 name="trash-alt" size={16} color='gray'/>
-  //     </TouchableOpacity>
-  //   </View>
-  // )
-  // }
-  
-  {/* <FlatList
-          data={task}
-          keyExtractor={(item: any) => item}
-          renderItem={( item: { id: any; title: any; } ) => (
-            <Task
-            key={item.id}
-              name={item.title}
-              onRemove={() => handleTaskRemove()}
-              onCheck={() => handleTaskCheck()}
-              />
-              )}
-              showsVerticalScrollIndicator={false}
-              ListEmptyComponent={() => (
-                <View style={styles.listEmpty}>
-                <FontAwesome5 name="clipboard-list" size={60} color="#808080" />
-                <Text style={styles.listEmptyTitle}>
-                Você ainda não tem tarefas cadastradas
-                </Text>
-                <Text style={styles.listEmptySubtitle}>
-                Crie tarefas e organize seus itens a fazer
-                </Text>
-                </View>
-                )}
-              /> */}
-
-              
-
 import React from 'react';
 import { FlatList, TouchableOpacity, View, Text } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import Icon  from 'react-native-vector-icons/Feather';
 import { styles } from './style';
 import { ItemWrapper } from '../ItemWrapper';
 
@@ -68,12 +23,23 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
     <FlatList
       data={tasks}
       keyExtractor={item => String(item.id)}
-      contentContainerStyle={{ paddingBottom: 24 }}
+      contentContainerStyle={ styles.contentContainer}
       showsVerticalScrollIndicator={false}
+      ListEmptyComponent={() => (
+        <View style={styles.listEmpty}>
+        <FontAwesome5 name="clipboard-list" size={60} color="#808080" />
+        <Text style={styles.listEmptyTitle}>
+        Você ainda não tem tarefas cadastradas
+        </Text>
+        <Text style={styles.listEmptySubtitle}>
+        Crie tarefas e organize seus itens a fazer
+        </Text>
+        </View>
+        )}
       renderItem={({ item, index }) => {
         return (
           <ItemWrapper index={index}>
-            <View>
+            <View style={styles.containerTask}>
               <TouchableOpacity
                 activeOpacity={0.7}
                 style={styles.taskButton}
@@ -84,7 +50,11 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
                 >
                   { item.done && 
                   (
-                    <FontAwesome5 name="circle" size={20} color="#4EA8DE" />
+                    <Icon 
+                      name="check"
+                      size={12}
+                      color="#FFF"
+                    />
                   )
                   }
                 </View>
@@ -108,7 +78,7 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
         )
       }}
       style={{
-        marginTop: 32
+        backgroundColor:'#1A1A1A',
       }}
     />
   )
--- src/components/TasksList/style.ts
+++ src/components/TasksList/style.ts
@@ -14,22 +14,61 @@ export const styles = StyleSheet.create({
     borderRadius: 8,
     marginBottom: 8,
   },
-  taskText: {
+  containerTask: {
     width: '80%',
-    color: "#F2F2F2",
-    fontSize: 14,
-    fontWeight: "400",
-    height: 48,
-    marginLeft: 12,
-    marginRight: 12,
   },
-  buttonCheck: {},
-  buttonRemove: {
-    width: 32,
-    height: 32,
+  contentContainer:{
+    paddingBottom: 24
+  },
+  taskButton: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingVertical: 15,
+    marginBottom: 4,
     borderRadius: 4,
-    backgroundColor: "#333333",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  taskMarker: {
+    height: 16,
+    width: 16,
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#B2B2B2',
+    marginRight: 15,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  taskText: {
+    color: '#FFF',
+  },
+  taskMarkerDone: {
+    height: 16,
+    width: 16,
+    borderRadius: 4,
+    backgroundColor: '#1DB863',
+    marginRight: 15,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  taskTextDone: {
+    color: '#1DB863',
+    textDecorationLine: 'line-through',
+  },
+  listEmpty:{
+    paddingTop: 48,
+    paddingBottom: 48,
+    flex: 1,
+    flexDirection:'column',
+    alignItems:'center',
+  },
+  listEmptyTitle:{
+    color: 'gray',
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  listEmptySubtitle:{
+    color: 'gray',
+    fontSize: 14,
   },
 });
