f137ef4cb4fa0b155d12d24eb29626291d8e95c2
adautomoises <adautomaleandro@gmail.com>
2022-08-21 16:59:49 -0300

    Updates

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,13 @@
 import { View } from 'react-native';
 import { styles } from './style';
 import React, { useState } from 'react';
-
 import { Header } from '../../components/Header';
 import { Task, TasksList } from '../../components/TasksList';
 import { TodoInput } from '../../components/TodoInput';
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const textDoneCount = tasks.filter(item => item.done === true);
   
   function handleAddTask(newTaskTitle: string) {
     const newTask = {
@@ -15,19 +15,25 @@ export function Home() {
       title: newTaskTitle,
       done: false
     }
-
     setTasks(prevState => [...prevState, newTask])
   }
   function handleToggleTaskDone(id: number) {
+    const updatedTasks = tasks.map(task => ({...task}));
+    const foundItem = updatedTasks.find(item => item.id === id);
+    if(!foundItem){
+      return;
+    }
+    foundItem.done = !foundItem.done;
+    setTasks(updatedTasks);
   }
   function handleRemoveTask(id: number) {
-    const uptadeTask = tasks.filter(task => task.id !== id);
-    setTasks(uptadeTask);
+    const updatedTask = tasks.filter(task => task.id !== id);
+    setTasks(updatedTask);
   }
 
   return (
     <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+      <Header tasksCounter={tasks.length} tasksDone={textDoneCount.length} />
 
       <TodoInput addTask={handleAddTask} />
 
